7e3d81024ea38f386c5d8a084317b743fb70fcbd
TeaDove <peter.ibragimov@gmail.com>
2025-12-24 03:06:22 +0400

    nightly minor edit from TeaDoves-MacBook-Air-M4.local

--- pkg/nekoproxy/proxy.go
+++ pkg/nekoproxy/proxy.go
@@ -2,10 +2,12 @@ package nekoproxy
 
 import (
 	"context"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"sync/atomic"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/teadove/teasutils/utils/must_utils"
@@ -29,9 +31,20 @@ func (r *Proxy) SetTarget(target *url.URL) {
 }
 
 func (r *Proxy) MakeSTDProxy(ctx context.Context) *httputil.ReverseProxy {
+	dialer := net.Dialer{Timeout: 3 * time.Minute, KeepAlive: 3 * time.Minute}
+
 	reverseProxy := httputil.ReverseProxy{
 		Director: func(req *http.Request) {
 			rewriteRequestURL(req, r.target.Load())
+		},
+		Transport: &http.Transport{
+			Proxy:                 http.ProxyFromEnvironment,
+			DialContext:           (&dialer).DialContext,
+			ForceAttemptHTTP2:     false, // Because safari for some reason works bad with HTTP2 and web-sockets
+			MaxIdleConns:          200,
+			IdleConnTimeout:       10 * time.Minute,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
 		},
 	}
 
